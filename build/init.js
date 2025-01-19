bc850cf397f013c16dcaeadddce0191af43d255e
Ossama Jouini <skymen@users.noreply.github.com>
2025-01-19 16:21:30 +0900

    Better project initialisation

---
+++ build/init.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import open from "open";
+import { execSync } from "child_process";
+import * as chalkUtils from "./chalkUtils";
+import fromConsole from "./fromConsole";
+
+export default function initialiseProject() {
+  console.log(execSync("npm install"));
+  console.log(execSync("npm run updateProjectData"));
+  if (!fs.existsSync(".git")) {
+    console.log(execSync("git init"));
+    console.log(execSync("git add -A"));
+    console.log(execSync('git commit -m "Initial commit"'));
+    open("https://github.com/new");
+  }
+}
+
+if (fromConsole(import.meta.url)) {
+  chalkUtils.fromCommandLine();
+  initialiseProject();
+}
